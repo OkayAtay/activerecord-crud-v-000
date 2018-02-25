02dc6fb864cfe0e884f11af6b2971715b9656b5d
Araba Taylor <araba.taylor20@gmail.com>
2018-02-25 19:51:06 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block(name, year)
   Movie.create do |m|
-    m.title = name
-    m.release_date = year
+    m.title = "Home Alone"
+    m.release_date = 1990
   end
 end
 
