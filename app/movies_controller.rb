ff7d81e2f2528018d335462c08ad2167cc8847ba
Araba Taylor <araba.taylor20@gmail.com>
2018-02-25 18:59:42 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,9 +20,6 @@ def can_be_created_with_a_hash_of_attributes(attributes)
       in_theaters: false
   }
   movie = Movie.new(attributes)
-    attributes do |key, value|
-      movie.key = value
-    end
 end
 
 def can_be_created_in_a_block
