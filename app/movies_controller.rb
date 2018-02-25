2ccc210b7b0cc064431e415eebcefe11ac1ea703
Araba Taylor <araba.taylor20@gmail.com>
2018-02-25 20:06:54 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -72,7 +72,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie = Movie.last
-  __
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
